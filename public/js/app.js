9684a175e2dcdd9ec7fb2e2938df44fb8ddb5065
AElmoznino <andrewlaufer@hotmail.com>
2016-05-31 12:31:32 +0300

    Started playing with stacked bar charts

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,17 @@
-var data = [10, 6, 16, 23, 42];
+// var data = [10, 6, 16, 23, 42];
+var data = d3.csv("../../data/fanco-sales.csv");
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+var stack = d3.stack()
+  .keys()
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return "Sales:" + d; });
+
+// var x = d3.scale.linear()
+//     .domain([0, d3.max(data)])
+//     .range([0, 420]);
+
+// d3.select(".chart")
+//   .selectAll("div")
+//     .data(data)
+//   .enter().append("div")
+//     .style("width", function(d) { return x(d) + "px"; })
+//     .text(function(d) { return "Sales:" + d; });

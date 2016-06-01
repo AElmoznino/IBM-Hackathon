cd7be40da0b6f8b0726a3068aa039b0f78734865
Rony Rosenberg <ronyrosenberg@MacBook-Pro-de-Rony.local>
2016-06-01 10:58:01 +0300

    got sample and arrays from our data

--- public/js/controllers/d3Controller.js
+++ public/js/controllers/d3Controller.js
@@ -4,58 +4,107 @@ angular.module('fanCo')
       link: function(scope, element, attrs) {
         d3Service.d3().then(function(d3) {
 
+          //our data
           var data = sales.salesData;
+
+          //arrays for storing the specific data
+          dateArray = [];
+          productArray = [];
+          salesArray = [];
+
+          var getDate = function() { //we get the date and push into it's array
+            for(var i = 0; i < 3; i++) { //loop through data
+              for (var prop in data[i]) { //loop through the objects of data
+                // console.log(data[i]['Week Of']);
+                var date = data[i]['Week Of'];
+                if (prop === 'Week Of') {
+                  dateArray.push(date);
+                }
+              }
+            }
+          }
+          getDate(); 
+
+          var getSku = function() { //we get the date and push into it's array
+            for(var i = 0; i < 3; i++) { //loop through data
+              for (var prop in data[i]) { //loop through the objects of data
+                var sku = data[i]['SKU'];
+                if (prop === 'SKU') {
+                  productArray.push(sku); 
+                }
+              }
+            }
+          }
+          getSku();
+
+          var getSales = function() { //we get the date and push into it's array
+            for(var i = 0; i < 3; i++) { //loop through data
+              for (var prop in data[i]) { //loop through the objects of data
+                var sales = data[i]['Sales (Units)'];
+
+                if (prop === 'Sales (Units)') {
+                  salesArray.push(sales); 
+                }
+                
+              }
+            }
+          }
+          getSales();
+          console.log(salesArray)
+          console.log(productArray)
+          console.log(dateArray)
+
           // // console.log(sales.salesData)
 
-          var margin = {top: 20, right: 40, bottom: 40, left:50},
-              width = 900,
-              height = 500;
+          // var margin = {top: 20, right: 40, bottom: 40, left:50},
+          //     width = 900,
+          //     height = 500;
 
-          var x = d3.time.scale()
-              .domain([new Date(data[0]['Week Of']), d3.time.day.offset(new Date(data[1]['Week Of']), 1)])
-              .rangeRound([0, width - margin.left - margin.right]);
-              console.log(data[data.length - 1]['Week Of'])
-          var y = d3.scale.linear()
-              .domain([0, d3.max(data, function(d) { return d['Sales (Units)']; })])
-              .range([height - margin.top - margin.bottom, 0]);
+          // var x = d3.time.scale()
+          //     .domain([new Date(data[0]['Week Of']), d3.time.day.offset(new Date(data[1]['Week Of']), 1)])
+          //     .rangeRound([0, width - margin.left - margin.right]);
+          //     console.log(data[data.length - 1]['Week Of'])
+          // var y = d3.scale.linear()
+          //     .domain([0, d3.max(data, function(d) { return d['Sales (Units)']; })])
+          //     .range([height - margin.top - margin.bottom, 0]);
 
-          var xAxis = d3.svg.axis()
-              .scale(x)
-              .orient('bottom')
-              .ticks(d3.time.days, 1)
-              .tickFormat(d3.time.format(' %d %m %y'))
-              .tickSize(0)
-              .tickPadding(8);
+          // var xAxis = d3.svg.axis()
+          //     .scale(x)
+          //     .orient('bottom')
+          //     .ticks(d3.time.days, 1)
+          //     .tickFormat(d3.time.format(' %d %m %y'))
+          //     .tickSize(0)
+          //     .tickPadding(8);
 
-          var yAxis = d3.svg.axis()
-              .scale(y)
-              .orient('left')
-              .tickPadding(8);
+          // var yAxis = d3.svg.axis()
+          //     .scale(y)
+          //     .orient('left')
+          //     .tickPadding(8);
 
-          var svg = d3.select('.chart').append('svg')
-              .attr('class', 'chart')
-              .attr('width', width)
-              .attr('height', height)
-            .append('g')
-              .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+          // var svg = d3.select('.chart').append('svg')
+          //     .attr('class', 'chart')
+          //     .attr('width', width)
+          //     .attr('height', height)
+          //   .append('g')
+          //     .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-          svg.selectAll('.chart')
-              .data(data)
-              .enter().append('rect')
-              .attr('class', 'bar')
-              .attr('x', function(d) { return x(new Date(d['Week Of'])); })
-              .attr('y', function(d) { return height - margin.top - margin.bottom - (height - margin.top - margin.bottom - y(d['Sales (Units)'])) })
-              .attr('width', 10)
-              .attr('height', function(d) { return height - margin.top - margin.bottom - y(d['Sales (Units)']) });
+          // svg.selectAll('.chart')
+          //     .data(data)
+          //     .enter().append('rect')
+          //     .attr('class', 'bar')
+          //     .attr('x', function(d) { return x(new Date(d['Week Of'])); })
+          //     .attr('y', function(d) { return height - margin.top - margin.bottom - (height - margin.top - margin.bottom - y(d['Sales (Units)'])) })
+          //     .attr('width', 10)
+          //     .attr('height', function(d) { return height - margin.top - margin.bottom - y(d['Sales (Units)']) });
 
-          svg.append('g')
-              .attr('class', 'x axis')
-              .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
-              .call(xAxis);
+          // svg.append('g')
+          //     .attr('class', 'x axis')
+          //     .attr('transform', 'translate(0, ' + (height - margin.top - margin.bottom) + ')')
+          //     .call(xAxis);
 
-          svg.append('g')
-            .attr('class', 'y axis')
-            .call(yAxis);
+          // svg.append('g')
+          //   .attr('class', 'y axis')
+          //   .call(yAxis);
 
           // axis
           // var w = 500;

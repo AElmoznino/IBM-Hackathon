128dad52191d993e9cc0204c353b7aa8660cf4c0
Rony Rosenberg <ronyrosenberg@mbp-de-rony.mynet>
2016-06-05 10:45:21 +0300

    improved

--- public/js/directives/revenueDirective.js
+++ public/js/directives/revenueDirective.js
@@ -106,83 +106,167 @@ angular
                     bottom: 20,
                     left: 50
                 }
-            var chart = d3.select('.line').append('svg').attr({
-                        width: w,
-                        height: h,
-                        margins: margins,
-            });                        
-            
 
+            function buildLine(d) {    
+                  var chart = d3.select('.line').append('svg').attr({
+                              width: w,
+                              height: h,
+                              margins: margins,
+                  });                        
+                  
+                  var xScale = d3.time.scale()
+                                  .domain([resultByDate[0].date, resultByDate[resultByDate.length-1].date])
+                                  .range([margins.left, w - margins.right])
+                  var yScale = d3.scale.linear()
+                                   .domain([0, d3.max(result.map(function (d) {
+                                    //  console.log(d.sum);
+                                      return d.sum;
+                                    }))])
+                                   .range([h - margins.top, margins.bottom])
+                  
+                  var xAxisGen = d3.svg
+                                .axis()
+                                .scale(xScale)
+                                .ticks(12)
+                                .tickSize(1)
+                                .tickFormat(d3.time.format('%b %y'))                             
+                  
+                  var yAxisGen = d3.svg
+                                .axis()
+                                .scale(yScale)
+                                .ticks(20)
+                                .tickSize(1)
+                                .orient('left');
 
-            var xScale = d3.time.scale().range([margins.left, w - margins.right])
-                            .domain([resultByDate[0].date, resultByDate[resultByDate.length-1].date])
-            var yScale = d3.scale.linear().range([h - margins.top, margins.bottom])
-                             .domain([0, d3.max(result.map(function (d) {
-                              //  console.log(d.sum);
-                                return d.sum;
-                              }))])
-            var xAxis = d3.svg
-                          .axis()
-                          .scale(xScale)
-                          .ticks(12)
-                          .tickSize(1)
-                          .tickFormat(d3.time.format('%b %y'))                             
-            
-            var yAxis = d3.svg
-                          .axis()
-                          .scale(yScale)
-                          .ticks(20)
-                          .tickSize(1)
-                          .orient('left');
+                    
 
-              
-
-                chart.attr({
-                  'width': w + margins.left + margins.right,
-                  'height': h + margins.top + margins.bottom
-                })
-
-                chart.append('svg:g')
-                     .attr({
-                        'class': 'x axis',
-                        'transform': 'translate(0,' + (h - margins.bottom) + ')',
-                        'shape-rendering': 'crispEdges'
+                      chart.attr({
+                        'width': w + margins.left + margins.right,
+                        'height': h + margins.top + margins.bottom
                       })
-                     .call(xAxis)
 
-                chart.selectAll('.x text')  // select all the text elements for the xaxis
-                      .attr('transform', function(d) {
-                        console.log( this.getBBox());
-                        return 'translate(' + this.getBBox().height + ',' + this.getBBox().height + ')rotate(-45)';
-                      });
+                      var xAxis = chart.append('g').call(xAxisGen)
+                           .attr({
+                              'class': 'x-axis',
+                              'transform': 'translate(0,' + (h - margins.bottom) + ')',
+                              'shape-rendering': 'crispEdges'
+                            })
 
-                chart.append('svg:g')
-                     .attr({
-                        'class': 'y axis',
-                        'transform': 'translate(' + (margins.left) + ',0)',
-                        'shape-rendering':'crispEdges'
+                      var yAxis = chart.append('g').call(yAxisGen)
+                            .attr({
+                              'class': 'y-axis',
+                              'transform': 'translate(' + (margins.left) + ',0)',
+                              'shape-rendering':'crispEdges'
+                            })
+                           
+
+                      chart.selectAll('.x-axis text')  // select all the text elements for the xaxis
+                            .attr('transform', function(d) {
+                              console.log( this.getBBox());
+                              return 'translate(' + this.getBBox().height + ',' + this.getBBox().height + ')rotate(-45)';
+                            });
+
+                      var lineGen = d3.svg.line()
+                           .x(function(d) {
+                             return xScale(new Date(d.date));
+                           })
+                           .y(function(d) {
+                             return yScale(Number(d.sum));
+                           })
+                           .interpolate('basis');
+
+
+                      var viz = chart.append('svg:path')
+                            .attr({
+                              'd': lineGen(result),
+                              'stroke': 'green',
+                              'stroke-width': 1,
+                              'fill': 'none'
+                            });
+              }//end function buildLine
+
+              //function for updating the chart according to the user's selection
+              function updateLine(result){
+                 var chart = d3.select('.line').append('svg').attr({
+                              width: w,
+                              height: h,
+                              margins: margins,
+                  });                        
+                  
+                  var xScale = d3.time.scale()
+                                  .domain([resultByDate[0].date, resultByDate[resultByDate.length-1].date])
+                                  .range([margins.left, w - margins.right])
+
+                  var yScale = d3.scale.linear().range([h - margins.top, margins.bottom])
+                                   .domain([0, d3.max(result.map(function (d) {
+                                    //  console.log(d.sum);
+                                      return d.sum;
+                                    }))])
+                  
+                  var xAxisGen = d3.svg
+                                .axis()
+                                .scale(xScale)
+                                .ticks(12)
+                                .tickSize(1)
+                                .tickFormat(d3.time.format('%b %y'))                             
+                  
+                  var yAxisGen = d3.svg
+                                .axis()
+                                .scale(yScale)
+                                .ticks(20)
+                                .tickSize(1)
+                                .orient('left');
+
+                    
+
+                      chart.attr({
+                        'width': w + margins.left + margins.right,
+                        'height': h + margins.top + margins.bottom
                       })
-                     .call(yAxis)
 
-                var lineGen = d3.svg.line()
-                     .x(function(d) {
-                       return xScale(new Date(d.date));
-                     })
-                     .y(function(d) {
-                       return yScale(Number(d.sum));
-                     })
-                     .interpolate('basis');
+                      var xAxis = chart.append('g').call(xAxisGen)
+                           .attr({
+                              'class': 'x-axis',
+                              'transform': 'translate(0,' + (h - margins.bottom) + ')',
+                              'shape-rendering': 'crispEdges'
+                            })
+
+                      var yAxis = chart.append('g').call(yAxisGen)
+                            .attr({
+                              'class': 'y-axis',
+                              'transform': 'translate(' + (margins.left) + ',0)',
+                              'shape-rendering':'crispEdges'
+                            })
+                           
+
+                      chart.selectAll('.x-axis text')  // select all the text elements for the xaxis
+                            .attr('transform', function(d) {
+                              console.log( this.getBBox());
+                              return 'translate(' + this.getBBox().height + ',' + this.getBBox().height + ')rotate(-45)';
+                            });
+
+                      var lineGen = d3.svg.line()
+                           .x(function(d) {
+                             return xScale(new Date(d.date));
+                           })
+                           .y(function(d) {
+                             return yScale(Number(d.sum));
+                           })
+                           .interpolate('basis');
 
 
-                chart.append('svg:path')
-                     .attr({
-                        'd': lineGen(result),
-                        'stroke': 'green',
-                        'stroke-width': 1,
-                        'fill': 'none'
-                      });
+                      var viz = chart.append('svg:path')
+                            .attr({
+                              'd': lineGen(result),
+                              'stroke': 'green',
+                              'stroke-width': 1,
+                              'fill': 'none'
+                            });
+              } //end function updateLine
 
-          }); //end chart
+
+              buildLine(resultByDate)
+          }); 
         }
       }
   }]);

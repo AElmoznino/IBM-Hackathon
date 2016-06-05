9fcc6235f6a6f58587c091defcc1406fa2961725
Rony Rosenberg <ronyrosenberg@mbp-de-rony.mynet>
2016-06-05 10:08:21 +0300

    rearrange of d3 directive

--- public/js/directives/revenueDirective.js
+++ public/js/directives/revenueDirective.js
@@ -98,56 +98,71 @@ angular
             var result = combine(data),
                 resultByDate = sort(result, true);
 
-            var chart = d3.select('.line').append('svg'),
-                width = 900,
-                height = 500,
-                margins = {
+            var h = 500;
+            var w = 800;
+            var margins ={
                     top: 20,
                     right: 20,
                     bottom: 20,
                     left: 50
-                },
-                xScale = d3.time.scale().range([margins.left, width - margins.right])
-                                .domain([resultByDate[0].date, resultByDate[resultByDate.length-1].date]),
-                yScale = d3.scale.linear().range([height - margins.top, margins.bottom])
-                                 .domain([0, d3.max(result.map(function (d) {
-                                  //  console.log(d.sum);
-                                    return d.sum;
-                                  }))]),
-                xAxis = d3.svg
+                }
+            var chart = d3.select('.line').append('svg').attr({
+                        width: w,
+                        height: h,
+                        margins: margins,
+            });                        
+            
+
+
+            var xScale = d3.time.scale().range([margins.left, w - margins.right])
+                            .domain([resultByDate[0].date, resultByDate[resultByDate.length-1].date])
+            var yScale = d3.scale.linear().range([h - margins.top, margins.bottom])
+                             .domain([0, d3.max(result.map(function (d) {
+                              //  console.log(d.sum);
+                                return d.sum;
+                              }))])
+            var xAxis = d3.svg
                           .axis()
                           .scale(xScale)
                           .ticks(12)
                           .tickSize(1)
-                          .tickFormat(d3.time.format('%b %y')),
-
-                yAxis = d3.svg
+                          .tickFormat(d3.time.format('%b %y'))                             
+            
+            var yAxis = d3.svg
                           .axis()
                           .scale(yScale)
                           .ticks(20)
                           .tickSize(1)
                           .orient('left');
 
-                chart.attr('width', width + margins.left + margins.right)
-                     .attr('height', height + margins.top + margins.bottom);
+              
+
+                chart.attr({
+                  'width': w + margins.left + margins.right,
+                  'height': h + margins.top + margins.bottom
+                })
 
                 chart.append('svg:g')
-                     .attr('class', 'x axis')
-                     .attr('transform', 'translate(0,' + (height - margins.bottom) + ')')
+                     .attr({
+                        'class': 'x axis',
+                        'transform': 'translate(0,' + (h - margins.bottom) + ')',
+                        'shape-rendering': 'crispEdges'
+                      })
                      .call(xAxis)
-                     .attr('shape-rendering','crispEdges');
 
                 chart.selectAll('.x text')  // select all the text elements for the xaxis
                       .attr('transform', function(d) {
-                        // console.log( this.getBBox());
+                        console.log( this.getBBox());
                         return 'translate(' + this.getBBox().height + ',' + this.getBBox().height + ')rotate(-45)';
                       });
 
                 chart.append('svg:g')
-                     .attr('class', 'y axis')
-                     .attr('transform', 'translate(' + (margins.left) + ',0)')
+                     .attr({
+                        'class': 'y axis',
+                        'transform': 'translate(' + (margins.left) + ',0)',
+                        'shape-rendering':'crispEdges'
+                      })
                      .call(yAxis)
-                     .attr('shape-rendering','crispEdges');
 
                 var lineGen = d3.svg.line()
                      .x(function(d) {
@@ -160,12 +175,14 @@ angular
 
 
                 chart.append('svg:path')
-                     .attr('d', lineGen(result))
-                     .attr('stroke', 'green')
-                     .attr('stroke-width', 1)
-                     .attr('fill', 'none');
+                     .attr({
+                        'd': lineGen(result),
+                        'stroke': 'green',
+                        'stroke-width': 1,
+                        'fill': 'none'
+                      });
 
-          });
+          }); //end chart
         }
       }
   }]);

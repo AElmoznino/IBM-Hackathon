c2786b0315ef68fa415f3874113c79ccf47a623b
Leonid <golani04@gmail.com>
2016-06-06 13:05:29 +0300

    add comments

--- public/js/services/sales.js
+++ public/js/services/sales.js
@@ -20,15 +20,20 @@ angular
       },
 
       getJoinSalesAndPrices: function () {
+        // using $q to resolve a promise
         var defer = $q.defer();
         var that = this;
+        // using promise to be sure that I get dataand after it displaying data
         $q.all([
+          //assuring that the data already stored
           $q.when(this.getAllSales()),
           $q.when(this.getPrices())
         ])
+        // after succesful call I'm doing some manipulation with object
         .then(function () {
-          console.log(that.pricesData);
+          // creating a new object to store specific data
           var data = [];
+          // looping over 2 objects and combining data into one
           for (var i = 0; i < that.pricesData.length; i++) {
             angular.forEach(that.salesData, function (val) {
               if (that.pricesData[i]['SKU'] === val['SKU'] && val['Channel'] === that.pricesData[i]['Channel']) {
@@ -37,6 +42,7 @@ angular
             });
           }
 
+          //sending to directives data with specific elements
           angular.forEach(that.salesData, function (val, key) {
             data.push({
                 'Week Of': val['Week Of'],
@@ -48,13 +54,15 @@ angular
                 'Revenue Per Unit Sold ($)': val['Revenue Per Unit Sold ($)']
               });
           });
-
+          // resolving a promise
           defer.resolve(data);
        }, function (err) {
+         // rejecting if happened something on the way
          defer.reject(err);
          console.log(err);
        });
 
+       //sending back promise
        return defer.promise;
      }
    }

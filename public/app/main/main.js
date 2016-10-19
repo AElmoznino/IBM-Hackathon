47c4dd5cde48710d39f3874b4c312a18226c83dc
AElmoznino <andrewlaufer@hotmail.com>
2016-10-19 18:02:28 +0200

    Quick fix

--- public/app/main/main.js
+++ public/app/main/main.js
@@ -48675,7 +48675,7 @@ module.exports = function ($http, $q) {
 
     getForecast: function() {
       var deferred = $q.defer();
-      var apiKey = '871830d2ca37d894c7ce25994ee70244',
+      var apiKey = '9711d482a429070923e6dbb923fb2012',
         lat = '42.3587',
         lon = '-71.0567',
         url = ['https://api.forecast.io/forecast/', apiKey, '/', lat, ',', lon, '?exclude=currently,minutely,hourly,flags&units=si&callback=JSON_CALLBACK'].join('');
